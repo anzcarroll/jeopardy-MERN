ba9f7c0b428c4d24a69f30eba8403c0c39623bfb
anzcarroll <anzcarroll@gmail.com>
2017-08-10 09:08:54 -0400

    depolyed to heroku...but its not working...so..figure that out

--- server.js
+++ server.js
@@ -1,32 +1,33 @@
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const GamesController = require("./controllers/game");
 const app = express();
-const GamesController = require('./controllers/game');
+
 mongoose.Promise = global.Promise;
-//mongoose
+
+// Mongoose Connection
 mongoose.connect(process.env.MONGODB_URI);
 const connection = mongoose.connection;
 
 connection.on('connected', () => {
-    console.log("mongoose successfully connected");
-})
-connection.on('error', (err) => {
-    console.log("mongoose connection error" + err);
+  console.log("Mongoose Connected Successfully");
 })
 
+connection.on('error', (err) => {
+  console.log("Mongoose connection error: " + err);
+})
 
 app.use(bodyParser.json());
-app.use(express.static(_dirname +'/client/buidl'));
+app.use(express.static(__dirname + '/client/build/'));
 
-app.use('/api/game', GamesController);
-
-app.get("/", (req,res) =>{
-    res.sendFile(_dirname + 'client/build/index.html')
-})
+app.use("/api/game", GamesController);
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/client/build/index.html");
+});
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log("App is listening on " + PORT)
-})
+  console.log("App is listening on: " + PORT);
+});
